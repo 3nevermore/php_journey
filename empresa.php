ef622db54c167c693040fcd565fe705a41804db2
Bruno <bruno_khalitzburg@hotmail.com>
2015-11-27 14:40:01 -0200

    commit inicial

---
+++ empresa.php
@@ -0,0 +1,11 @@
+
+
+<!-- Navigation -->
+<?php require_once 'includes/header.php' ;?>
+
+<!-- Content -->
+<?php require_once 'includes/content_outro.php' ;?>
+<h1>Empresa</h1>
+
+<!-- Footer -->
+<?php require_once 'includes/footer.php' ;?>
---
+++ index.php
@@ -0,0 +1,10 @@
+<?php $active_home= true; ?>
+
+    <!-- Navigation -->
+        <?php require_once 'includes/header.php' ;?>
+
+    <!-- Content -->
+    <?php require_once 'includes/content.php' ;?>
+
+    <!-- Footer -->
+    <?php require_once 'includes/footer.php' ;?>
